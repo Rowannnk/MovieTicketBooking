efcc818e48bec3388531677e1fc9449e184a6bcb
rowan <u6511498@au.edu>
2025-07-01 23:12:46 +0700

    booking confirmation

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,6 +2,7 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 //need to sync with inngest (https://app.inngest.com/env/production/apps) after deploying on vercel
 
@@ -76,10 +77,46 @@ const deleteSeatsAndDeleteBooking = inngest.createFunction(
   }
 );
 
+//inngest function to send email when user books a show
+const sendBookingConfirmationEmail = inngest.createFunction(
+  { id: "send-booking-confirmation-email" },
+  { event: "app/show.booked" },
+  async ({ event, step }) => {
+    const { bookingId } = event.data;
+    const booking = await Booking.findById(bookingId)
+      .populate({
+        path: "show",
+        populate: {
+          path: "movie",
+          model: "Movie",
+        },
+      })
+      .populate("user");
+    await sendEmail({
+      to: booking.user.email,
+      subject: `Payment Confirmation: "${booking.show.movie.title}" booked!`,
+      body: `<div>
+      <h2>Hi ${booking.user.name},</h2>
+      <p>Your booking for <strong> "${
+        booking.show.movie.title
+      }" </strong>is confirmed.
+      <p> 
+      <strong>Date:</strong>${new Date(
+        booking.show.showDateTime
+      ).toLocaleDateString("en-Us", { timeZone: "Asia/Thailand" })}<br />
+      </p>
+      <p>Enjoy the show!</p>
+      <p>Thanks for booking with us ! <br /> Rowan Movie Ticket </p>
+      </div>`,
+    });
+  }
+);
+
 // Create an empty array where we'll export future Inngest functions
 export const functions = [
   userCreation,
   userDeletion,
   userUpdation,
   deleteSeatsAndDeleteBooking,
+  sendBookingConfirmationEmail,
 ];
